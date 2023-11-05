bb89abaec2fb5c651585f3feed9a00e1c5133f05
Juliane <julianebueno1@gmail.com>
2023-11-05 08:53:20 -0300

    fix: :fire: remoção de arquivos com código repitido e/ou confuso

--- src/js/form.js
+++
@@ -1,4 +0,0 @@
-import * as script from "./model/construirHeaderFooter.js";
-
-script.construirHeader(false);
-script.construirFooter();
--- src/js/index.js
+++
@@ -1,4 +0,0 @@
-import * as script from "./model/construirHeaderFooter.js";
-
-script.construirHeader(true);
-script.construirFooter();
