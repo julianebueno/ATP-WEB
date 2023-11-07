8a9f98fc74aff2a39398aba3109bd15790558610
Juliane <julianebueno1@gmail.com>
2023-11-07 02:30:31 -0300

    feat: :sparkles: adição de patterns no form

--- src/js/controller/funcoesForm.js
+++ src/js/controller/funcoesForm.js
@@ -4,11 +4,13 @@ var contador = 2;
 
 function addInputIngredientes() {
   let input = document.createElement("input");
-  input.setAttribute('type', 'text');
-  input.setAttribute('name', `ingrediente${contador}`);
-  input.setAttribute('id', `ingrediente${contador}`);
-  input.setAttribute('placeholder', `Ingrediente ${contador}...`);
-  input.setAttribute('required', '');
+  input.setAttribute("type", "text");
+  input.setAttribute("name", `ingrediente${contador}`);
+  input.setAttribute("id", `ingrediente${contador}`);
+  input.setAttribute("placeholder", `Ingrediente ${contador}...`);
+  input.setAttribute("pattern", "[a-zA-ZÀ-ÿ0-9 ]+");
+  input.setAttribute("title", "Sem caracteres especias, insira apenas letras, números, espaços e acentos");
+  input.setAttribute("required", "");
   container.appendChild(input);
   contador++;
 }
@@ -19,11 +21,13 @@ var contador2 = 2;
 
 function addInputModoFazer() {
   let input2 = document.createElement("input");
-  input2.setAttribute('type', 'text');
-  input2.setAttribute('name', `passo${contador2}`);
-  input2.setAttribute('id', `passo${contador2}`);
-  input2.setAttribute('placeholder', `Passo ${contador2}...`);
-  input2.setAttribute('required', '');
+  input2.setAttribute("type", "text");
+  input2.setAttribute("name", `passoModoFazer${contador2}`);
+  input2.setAttribute("id", `passoModoFazer${contador2}`);
+  input2.setAttribute("placeholder", `Passo ${contador2}...`);
+  input2.setAttribute("pattern", "[a-zA-ZÀ-ÿ0-9 ]+");
+  input2.setAttribute("title", "Sem caracteres especias, insira apenas letras, números, espaços e acentos");
+  input2.setAttribute("required", "");
   container2.appendChild(input2);
   contador2++;
 }
