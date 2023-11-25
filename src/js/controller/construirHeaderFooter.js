40b5e1c2ae6bae34e1c20e3b6b80542e1e12a76c
Juliane <julianebueno1@gmail.com>
2023-11-25 06:59:50 -0300

    fix: :bug: reestruturação do header

--- src/js/controller/construirHeaderFooter.js
+++
@@ -1,9 +0,0 @@
-import * as script from "../model/HeaderFooter.js";
-
-const headerElementIndex = document.getElementById("headerIndex");
-if (headerElementIndex) script.construirHeaderIndex();
-
-const headerElementGeral = document.getElementById("header");
-if (headerElementGeral) script.construirHeaderGeral();
-
-script.construirFooter();
